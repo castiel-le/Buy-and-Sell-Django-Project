d65ded6a6eb1a8610a702bbd4992fa37e34203d4
Kirill Parhomenco <6976451-greenmayo@users.noreply.gitlab.com>
2021-04-20 13:13:01 -0400

    Created home and about html pages with blocks Created skeleton for blog and about using template inheritance - base.html

--- bns/blog/views.py
+++ bns/blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Kirill',
+        'title': 'First item',
+        'content': 'Used chair, good state',
+        'date_posted':  'April 19, 2021'
+    },
+    {
+        'author': 'John',
+        'title': 'Second item',
+        'content': 'Carpet, nice',
+        'date_posted':  'April 20, 2021'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>BNS Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>BNS About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
