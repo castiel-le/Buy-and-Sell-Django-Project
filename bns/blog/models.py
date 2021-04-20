136ca13e0988ec3d5b202ebc7c09c07da25a1dd6
Kirill Parhomenco <6976451-greenmayo@users.noreply.gitlab.com>
2021-04-20 19:29:49 -0400

    - added Post to admin page - added dataSources.xml (???) - comment for date format - created Post model in blog/models.py - added a production log for personal remarks and tips - linked blog/views.py to DB via Post model

--- bns/blog/models.py
+++ bns/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # to change the time to be constant after post creation, set DateTimeField to auto_now_add=True
+    date_posted = models.DateTimeField(default=timezone.now)
+    # CASCADE - if the user deleted, posts of that user are deleted too
+    # use SET() to pass an author (i.e. "author deleted")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- bns/blog/views.py
+++ bns/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Kirill',
-        'title': 'First item',
-        'content': 'Used chair, good state',
-        'date_posted':  'April 19, 2021'
-    },
-    {
-        'author': 'John',
-        'title': 'Second item',
-        'content': 'Carpet, nice',
-        'date_posted':  'April 20, 2021'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
