7ff12e512d2d88a455256e53cf6ceb262a0109fa
Kirill Parhomenco <6976451-greenmayo@users.noreply.gitlab.com>
2021-04-21 14:35:11 -0400

    Reformatted code Added login and logout views Changed base that we can see the header differently when logged in or not Created a profile view Added login redirect url as homepage and login_url as login Added url for login logout in urls root Made that when you register, you have to login after

--- bns/users/views.py
+++ bns/users/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+
 from .forms import UserRegisterForm
 
 
@@ -10,8 +12,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Account created, you now may log in as {username}!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required  # decorator to require a logged in user to see the profile.html
+def profile(request):
+    return render(request, 'users/profile.html')
