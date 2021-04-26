e56e42c57d644ab743072cf863b146a921596b05
Kirill Parhomenco <6976451-greenmayo@users.noreply.gitlab.com>
2021-04-26 16:55:24 -0400

    Added pagination Added ability to show only a user's posts Some cleanup and fixing broken links Attempt at doing password reset, too tired, thats for tomorrow

--- bns/blog/views.py
+++ bns/blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # views handle routes to html pages, generally speaking
@@ -23,7 +24,21 @@ class PostListView(ListView):  # to display homepage with posts
     # <app>/<model>_<viewtype>.html, but we can override
     #       and also set another variable so it does not look for 'list'
     ordering = ['-date_posted']  # this would put newer ads first
+    paginate_by = 10
 
+
+class UserPostListView(ListView):  # to display homepage with posts
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    # when class view used, it is looking for a specific pattern:
+    # <app>/<model>_<viewtype>.html, but we can override
+    #       and also set another variable so it does not look for 'list'
+    paginate_by = 10
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 # view for each individual post
 #
