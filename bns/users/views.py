388a7d46fcdf6239742fa290132210d619721d11
Kirill Parhomenco <6976451-greenmayo@users.noreply.gitlab.com>
2021-04-26 12:20:11 -0400

    Added a user profile uipdate form Played around with pillow library and added user profile image to posts and user can modify their profile image

--- bns/users/views.py
+++ bns/users/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -22,4 +22,23 @@ def register(request):
 @login_required  # decorator to require a logged in user to see the profile.html so when trying to access
 # the profile page directly without being logged in, it would ask to log in and redirect to profile after
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method =='POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        # info in brackets is to populate "update" field with current user data
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Account updated successfully!')
+            return redirect('profile')
+    #     returning a redirect is good because it sends us away from the page and there would be
+    #     no weird popups about resending the data because. =We dont want it to fall through all the way to render
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+    return render(request, 'users/profile.html', context)
