80badc09e501b2549651f954d0631cfe17061504
Nael Louis <nael.louis1002@gmail.com>
2021-05-20 22:54:03 -0400

    comments

--- bns/blog/urls.py
+++ bns/blog/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
-    path('post/<int:pk>/comment', views.post_detail_view, name='post-comment')
+    path('post/<int:pk>/comment', views.post_detail_view, name='post-comment'),
     path('post/<int:pk>/purchase', PurchaseItemView.as_view(), name='post-purchase'),
 ]
 
